4a8f1b706500d8f78f3edbe2a093a4eb00de3f9a
conkyliu <conkyliu@163.com>
2022-05-24 20:26:56 +0800

    添加自定义事件

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -3,9 +3,9 @@
  * @author wangfupeng
  */
 
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import '@wangeditor/editor/dist/css/style.css'
-import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import {Editor, Toolbar} from '@wangeditor/editor-for-react'
 
 function MyEditor() {
     const [editor, setEditor] = useState(null) // 存储 editor 实例
@@ -16,21 +16,30 @@ function MyEditor() {
         setTimeout(() => {
             setHtml('<p>hello&nbsp;<strong>world</strong>.</p>\n<p><br></p>')
         }, 1500);
-        editor?.on('change',html=>{console.log('-------html-----',html)});
-        
     }, [])
 
-    const toolbarConfig = { }
+    const toolbarConfig = {}
     const editorConfig = {
         placeholder: '请输入内容...',
     }
 
     // 及时销毁 editor
     useEffect(() => {
+        window.WEditor = editor;
+        console.log(editor, 999);
+        editor?.on('contentChange', (html) => {
+            console.log('=======contentChange html=======', html)
+        })
+        editor?.on('updateContent', (html) => {
+            console.log('======updateContent html=======', html)
+            setHtml(html)
+        })
+
         return () => {
             if (editor == null) return
             editor.destroy()
-            setEditor(null)
+            setEditor(null);
+
         }
     }, [editor])
 
@@ -51,26 +60,27 @@ function MyEditor() {
                 <button onClick={printHtml}>print html</button>
             </div>
 
-            <div style={{ border: '1px solid #ccc', zIndex: 100, marginTop: '15px'}}>
+            <div style={{border: '1px solid #ccc', zIndex: 100, marginTop: '15px'}}>
                 <Toolbar
                     editor={editor}
                     defaultConfig={toolbarConfig}
                     mode="default"
-                    style={{ borderBottom: '1px solid #ccc' }}
+                    style={{borderBottom: '1px solid #ccc'}}
                 />
                 <Editor
                     defaultConfig={editorConfig}
                     value={html}
                     onCreated={setEditor}
-                    onChange={editor => {
-                              setHtml(editor.getHtml());
-                              editor.emit('change',editor.getHtml());
-                              }}
+                    onChange={e => {
+                        let html = e.getHtml();
+                        setHtml(html);
+                        e?.emit('contentChange', html)
+                    }}
                     mode="default"
-                    style={{ height: '500px' }}
+                    style={{height: '500px'}}
                 />
             </div>
-            <div style={{ marginTop: '15px' }}>
+            <div style={{marginTop: '15px'}}>
                 {html}
             </div>
         </>
